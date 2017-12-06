9e2793e69a656995f232faca43254c20b0ccb702
Erik Kelemen <xkelem01@stud.fit.vutbr.cz>
2017-12-06 13:43:33 +0100

    rendering completed v0.1

--- xkelem01.asm
+++ xkelem01.asm
@@ -1,4 +1,4 @@
-section	.text
+section  .text
    global _start         ;must be declared for using gcc
 
 
@@ -10,6 +10,7 @@ section	.text
    extern move_side
 
    extern update_look
+   extern texture_create
 
    extern cos
    extern sin
@@ -50,10 +51,14 @@ section	.text
    extern glDrawElements
    extern glTexImage2D
    extern glTexCoordPointer
+   extern glGenerateMipmap
 
    extern glClear
    extern glMatrixMode
    extern glLoadIdentity
+   extern glPushMatrix
+   extern glTranslated
+   extern glPopMatrix
 
    extern gluLookAt
 
@@ -78,7 +83,10 @@ section	.text
 %define GL_FLOAT 0x1406
 %define GL_TRIANGLES 0x4
 %define GL_VERTEX_ARRAY 0x8074
+%define GL_NORMAL_ARRAY 0x8075
+%define GL_UNSIGNED_BYTE 0x1401
 %define GL_UNSIGNED_SHORT 0x1403
+%define GL_UNSIGNED_INT 0x1405
 %define GL_SHORT 0x1402
 %define GL_TEXTURE_2D 0xDE1
 %define GL_TEXTURE_WRAP_S 0x2802
@@ -132,22 +140,130 @@ struc Camera
 
 endstruc
 
+%macro open_file 2
+
+   mov rax, 5 ;open
+   mov rbx, %1
+   mov rcx, 0 ;read only
+   mov rdx, 0777 ;chmod
+   int  0x80
+
+   mov  [%2], rax
+
+%endmacro
+
+%macro close_file 1
+   mov eax, 6 ;close
+   mov ebx, [%1]
+   int 0x80
+%endmacro
+
+%macro read_file 1
+   mov rax, 3 ;read
+   mov rbx, [fd_in]
+   mov rcx, readed_data
+   mov rdx, %1
+   int 0x80
+%endmacro
+
+%macro read_file_in_rdx 0
+   mov rax, 3 ;read
+   mov rbx, [fd_in]
+   mov rcx, readed_data
+   int 0x80
+%endmacro
+
+%macro check_integrity 0   ;check sequence "xkelem01" in first 64 bits
+
+   read_file 16
+   mov rax, [readed_data]
+   mov rbx, [header]
+   cmp rax, rbx
+   jne end
+
+%endmacro
+
+%macro gen_buffer 1
+   mov rdi, 1 ;number
+   mov rsi, %1
+   call glGenBuffers
+%endmacro
+
+%macro buffer_data 3
+
+   mov rdi, GL_ARRAY_BUFFER
+   mov rsi, [%1]
+   call glBindBuffer
+
+   mov rdi, GL_ARRAY_BUFFER
+   mov rsi, %3 ;number of vertices
+   shl rsi, 5  ;;size in bytes (Vertices * 32)
+   mov rdx, %2 ;pointer
+   mov rcx, GL_STATIC_DRAW
+   call glBufferData
+
+%endmacro
+
+%macro element_buffer_data 3
+
+   mov rdi, GL_ELEMENT_ARRAY_BUFFER
+   mov rsi, [%1]  ;buffers_ids
+   call glBindBuffer
+
+   mov rdi, GL_ELEMENT_ARRAY_BUFFER
+   mov rsi, %3 ;number indices
+   shl rsi, 2 ;size in bytes
+   mov rdx, %2 ;pointer
+   mov rcx, GL_STATIC_DRAW
+   call glBufferData
+
+%endmacro
+   
+
 %macro opengl_texture_start 1
 
    mov rdi, GL_TEXTURE_2D
    call glEnable
 
-   mov rdi, %1
-   mov rsi, textures_ids
-   call glGenTextures
-
 %endmacro
 
 %macro load_texture 1
 
+   read_file 16
+   movapd xmm0, [readed_data]
+   movapd [textures_sizes + 16 + (%1*16)], xmm0
+
+   mov rdx, [textures_sizes + 16+(%1*16)]
+   mov rax, [textures_sizes + 24+(%1*16)]
+   mul edx
+   shl rdx, 32
+   mov edx, eax
+   mov rax, rdx
+   add rdx, rax
+   add rdx, rax
+
+   read_file_in_rdx
+
+   mov rdi, 1
+   mov rsi, textures_ids + (%1*8)
+   call glGenTextures
+
    mov rdi, GL_TEXTURE_2D
-   mov rsi, [textures_ids + %1]
+   mov rsi, [textures_ids + (%1*8)]
    call glBindTexture
+
+   mov rdi, GL_TEXTURE_2D
+   xor rsi, rsi
+   mov rdx, GL_RGB
+   mov rcx, [textures_sizes+16+(%1*16)]
+   mov r8, [textures_sizes+24+(%1*16)]
+   xor r9, r9  ;border must be 0
+   push readed_data
+   push GL_UNSIGNED_BYTE
+   push rdx
+
+   call glTexImage2D
+   sub rsp, 24
 
    mov rdi, GL_TEXTURE_2D
    mov rsi, GL_TEXTURE_WRAP_S
@@ -160,17 +276,23 @@ endstruc
    call glTexParameteri
 
    mov rdi, GL_TEXTURE_2D
-   xor rsi, rsi
-   mov rdx, GL_RGB
-   mov rcx, 2
-   mov r8, 2
-   xor r9, r9
-   push test_image
-   push GL_FLOAT
-   push rdx
+   call glGenerateMipmap
 
-   call glTexImage2D
-   sub rsp, 24
+%endmacro
+
+%macro read_texture 0
+
+   open_file texture_file, fd_in   ;open("textures.ipa");
+   check_integrity
+
+   movapd xmm0, [readed_data]
+   movapd [textures_sizes], xmm0
+
+   load_texture 0	;skybox
+   load_texture 1	;fling
+   load_texture 2	;grass
+
+   close_file fd_in  ;close("textures.ipa");
 
 %endmacro
 
@@ -205,7 +327,336 @@ endstruc
 
 %endmacro
 
-%macro load_meshes 1
+%macro load_meshes 0
+
+   open_file mesh_file, fd_in   ;open("meshes.ipa");
+   check_integrity
+
+   movapd xmm0, [readed_data]
+   movapd [buffers_sizes], xmm0
+
+   load_mesh 0 ;skybox
+   load_mesh 1 ;flint
+
+   close_file fd_in  ;close("meshes.ipa");
+
+%endmacro
+
+%macro load_mesh 1
+
+   gen_buffer buffers_ids+(16*%1)
+   gen_buffer buffers_ids+8+(16*%1)
+   read_file 16
+
+   movapd xmm1, [readed_data]
+   movapd [buffers_sizes+16+(16*%1)], xmm1
+
+   mov rdx, [readed_data]
+   shl rdx, 5
+
+   read_file_in_rdx
+
+   buffer_data buffers_ids+(16*%1), readed_data, [buffers_sizes+16+(16*%1)]
+
+   mov rdx, [buffers_sizes+24+(16*%1)]
+   shl rdx, 2
+
+   read_file_in_rdx
+
+   element_buffer_data buffers_ids+8+(16*%1), readed_data, [buffers_sizes+24+(16*%1)]
+
+%endmacro
+
+%macro read_terrain 0
+
+	mov rax, 3 ;read
+	mov rbx, [fd_in]
+	mov rcx, terrain_height
+	mov rdx, 65536 		; 128*128*sizeof(float) = 65536
+	int 0x80
+
+%endmacro
+
+%macro load_terrain 0
+
+   open_file terrain_file, fd_in   ;open("terrain.ipa");
+   read_terrain
+   close_file fd_in  ;close("terrain.ipa");
+
+   gen_buffer buffers_ids+(16*3)
+   gen_buffer buffers_ids+8+(16*3)
+
+   xor rcx,rcx
+   movapd xmm7, [mask3]
+   movapd xmm6, [terrain_size]
+   movapd xmm5, [var1]
+   xorpd xmm0, xmm0
+   lea rdx, [terrain_height]	;pointer to terrain memory
+   lea rbx, [readed_data]		;pointer to memory for graphic card
+
+   jmp terrain_continue
+
+terrain_new_row:
+
+	movapd xmm5, xmm6
+	shufps xmm5, xmm5, 225
+
+	shufps xmm0, xmm0, 231
+	addps xmm0, xmm5
+
+terrain_continue:
+   movapd xmm3, xmm0
+   movapd xmm1, [rdx]	;xmm1 = Z1|Z2|Z3|Z4
+   ;divps xmm1, xmm4
+   movapd xmm2, xmm1
+   andps xmm2, xmm7		;xmm2 = Z1|0.0|Z3|Z4
+
+   shufpd xmm3, xmm2, 0 	;xmm3 = x|y|z|0.0
+   movapd [rbx], xmm3
+
+   shufpd xmm5, xmm3, 0		;xmm5 = 0.0, 1.0, x, y
+   add rbx, 16
+   movapd [rbx], xmm5
+
+   addps xmm0, xmm6
+   movapd xmm3, xmm0
+   shufps xmm1, xmm1, 57	;rotate
+   movapd xmm2, xmm1
+   andps xmm2, xmm7		;xmm2 = Z2|0.0|Z4|Z1
+
+   shufpd xmm3, xmm2, 0
+   add rbx, 16
+   movapd [rbx], xmm3
+
+   shufpd xmm5, xmm3, 0		;xmm5 = 0.0, 1.0, x, y
+   add rbx, 16
+   movapd [rbx], xmm5
+
+   addps xmm0, xmm6
+   movapd xmm3, xmm0
+   shufps xmm1, xmm1, 57	;rotate
+   movapd xmm2, xmm1
+   andps xmm2, xmm7		;xmm2 = Z2|0.0|Z4|Z1
+
+   shufpd xmm3, xmm2, 0
+   add rbx, 16
+   movapd [rbx], xmm3
+
+   shufpd xmm5, xmm3, 0		;xmm5 = 0.0, 1.0, x, y
+   add rbx, 16
+   movapd [rbx], xmm5
+
+   addps xmm0, xmm6
+   movapd xmm3, xmm0
+   shufps xmm1, xmm1, 57	;rotate
+   movapd xmm2, xmm1
+   andps xmm2, xmm7		;xmm2 = Z2|0.0|Z4|Z1
+
+   shufpd xmm3, xmm2, 0
+   add rbx, 16
+   movapd [rbx], xmm3
+
+   shufpd xmm5, xmm3, 0		;xmm5 = 0.0, 1.0, x, y
+   add rbx, 16
+   movapd [rbx], xmm5
+
+   add rdx, 16
+   add rbx, 16
+   addps xmm0, xmm6
+   add rcx, 1
+   mov r10, 4096
+   cmp rcx, r10
+   jz terrain_end
+   mov rax, rcx
+   and rax, 31
+   mov r10, 0
+   cmp rax, r10
+   jz terrain_new_row
+   jmp terrain_continue
+
+terrain_end:
+
+	buffer_data buffers_ids+(16*3), readed_data, 16384
+
+	lea rcx, [readed_data]
+	mov rdx,1
+
+	movapd xmm3, [element_terrain_constant1]
+	movapd xmm4, [element_terrain_constant2]
+	movapd xmm5, [element_terrain_constant3]
+
+	movapd xmm6, [element_terrain_add]
+
+	jmp element_terrain_loop
+
+element_terrain_row:
+
+	add rdx, 1
+
+	paddd xmm3, xmm6
+	paddd xmm4, xmm6
+	paddd xmm5, xmm6
+
+element_terrain_loop:
+
+	movapd [rcx], xmm3
+	add rcx, 16
+	movapd [rcx], xmm4
+	add rcx, 16
+	movapd [rcx], xmm5
+	add rcx, 16
+
+	paddd xmm3, xmm6
+	paddd xmm4, xmm6
+	paddd xmm5, xmm6
+
+	add rdx, 1
+	mov rax, rdx
+	and rax, 63
+	cmp rax, 0
+	jz element_terrain_row
+	mov rax, 7998;8128
+	cmp rdx, rax
+	jz element_terrain_end
+	jmp element_terrain_loop
+
+element_terrain_end:
+
+	sub rcx, readed_data
+
+	element_buffer_data buffers_ids+8+(16*3), readed_data, 95976
+
+%endmacro
+
+%macro draw_terrain 0
+
+	bind_texture 2
+
+	mov rdi, GL_ARRAY_BUFFER
+	mov rsi, [buffers_ids+(16*3)]
+	call glBindBuffer
+
+	mov rdi, GL_ELEMENT_ARRAY_BUFFER
+	mov rsi, [buffers_ids+8+(16*3)]  ;buffers_ids
+	call glBindBuffer
+
+	setup_graphic_dataptr
+
+	mov rdi, GL_TRIANGLES
+	mov rsi, 95976
+	mov rdx, GL_UNSIGNED_INT
+	mov rcx, 0
+	call glDrawElements
+
+%endmacro
+
+%macro Debug_Vertex_Buffer 1
+
+	movss xmm0, [readed_data+(%1*32)]
+   call Debug_tool_float
+
+   movss xmm0, [readed_data+4+(%1*32)]
+   call Debug_tool_float
+
+   movss xmm0, [readed_data+8+(%1*32)]
+   call Debug_tool_float
+
+   movss xmm0, [readed_data+12+(%1*32)]
+   call Debug_tool_float
+
+   movss xmm0, [readed_data+16+(%1*32)]
+   call Debug_tool_float
+
+   movss xmm0, [readed_data+20+(%1*32)]
+   call Debug_tool_float
+
+   movss xmm0, [readed_data+24+(%1*32)]
+   call Debug_tool_float
+
+   movss xmm0, [readed_data+28+(%1*32)]
+   call Debug_tool_float
+
+   mov rdi, 99999
+   call Debug_tool_decimal
+%endmacro
+
+%macro Debug_Element_Buffer 1
+
+	mov edi, [readed_data+(%1*24)]
+	call Debug_tool_decimal
+
+	mov edi, [readed_data+4+(%1*24)]
+	call Debug_tool_decimal
+
+	mov edi, [readed_data+8+(%1*24)]
+	call Debug_tool_decimal
+
+	mov edi, [readed_data+12+(%1*24)]
+	call Debug_tool_decimal
+
+	mov edi, [readed_data+16+(%1*24)]
+	call Debug_tool_decimal
+
+	mov edi, [readed_data+20+(%1*24)]
+	call Debug_tool_decimal
+
+   mov rdi, 99999
+   call Debug_tool_decimal
+%endmacro
+
+%macro bind_texture 1
+   mov rdi, GL_TEXTURE_2D
+   mov rsi, [textures_ids+(8*%1)]
+   call glBindTexture
+%endmacro
+
+%macro draw_mesh 1
+   
+   mov rdi, GL_ARRAY_BUFFER
+   mov rsi, [buffers_ids+(16*%1)]
+   call glBindBuffer
+
+   mov rdi, GL_ELEMENT_ARRAY_BUFFER
+   mov rsi, [buffers_ids+8+(16*%1)]  ;buffers_ids
+   call glBindBuffer
+
+   setup_graphic_dataptr
+
+   mov rdi, GL_TRIANGLES
+   mov rsi, [buffers_sizes+24+(16*%1)]
+   mov rdx, GL_UNSIGNED_INT
+   mov rcx, 0
+   call glDrawElements
+
+%endmacro
+
+%macro setup_graphic_dataptr 0
+   
+   mov rdi, GL_VERTEX_ARRAY
+   call glEnableClientState
+
+   mov rdi, GL_NORMAL_ARRAY
+   call glEnableClientState
+
+   mov rdi, GL_TEXTURE_COORD_ARRAY
+   call glEnableClientState
+
+   mov rdi, 3
+   mov rsi, GL_FLOAT
+   mov rdx, 32
+   mov rcx, 0
+   call glVertexPointer
+
+   mov rdi, GL_FLOAT
+   mov rsi, 32
+   mov rdx, 12
+   call glNormalPointer
+
+   mov rdi, 2
+   mov rsi, GL_FLOAT
+   mov rdx, 32
+   mov rcx, 24
+   call glTexCoordPointer
 
 %endmacro
    
@@ -348,151 +799,35 @@ _start:
 
    ;Prepare buffers
 
-   mov rdi, 1 ;vertices
-   mov rsi, buffers_ids
-   call glGenBuffers
-
-   mov rdi, 1 ;normals
-   mov rsi, buffers_ids + 8
-   call glGenBuffers
-
-   mov rdi, 1 ;texture coords
-   mov rsi, buffers_ids + 16
-   call glGenBuffers
-
-   mov rdi, 1 ;indices
-   mov rsi, buffers_ids + 24
-   call glGenBuffers
-
    opengl_texture_start 1
 
-   load_texture 0
+   call _timer_start
 
-   ;Open file
+   read_texture
 
-   mov rax, 5 ;open
-   mov rbx, file_name
-   mov rcx, 0 ;read only
-   mov rdx, 0777 ;chmod
-   int  0x80
-   
-   mov  [fd_in], rax
+   mov rcx, func_name_Load_Textures
+   call _timer_stop
 
-   mov rax, 3 ;read
-   mov rbx, [fd_in]
-   mov rcx, readed_data
-   mov rdx, 8
-   int 0x80
+   ;load_texture 0
 
-   xor rdx, rdx
-   mov edx, [readed_data+4]
-   mov [buffers_sizes], rdx
+   ;call texture_create
 
-   shl rdx, 2
-   mov r10, rdx
-   add rdx, rdx
-   add rdx, r10
+   call _timer_start
 
-   mov r10, rdx
+   load_meshes
 
+   mov rcx, func_name_Load_Meshes
+   call _timer_stop
 
-   mov rax, 3 ;read
-   mov rbx, [fd_in]
-   mov rcx, readed_data
-   int 0x80
-
-   mov rdi, GL_ARRAY_BUFFER
-   mov rsi, [buffers_ids]
-   call glBindBuffer
-
-   mov rdi, GL_ARRAY_BUFFER
-   mov rsi, r10 ;size in bytes
-   mov rdx, readed_data
-   mov rcx, GL_STATIC_DRAW
-   call glBufferData
-
-   ;mov rdi, GL_ARRAY_BUFFER
-   ;mov rsi, 36 ;size in bytes
-   ;mov rdx, verticles
-   ;mov rcx, GL_STATIC_DRAW
-   ;call glBufferData
-
-   mov rdx, [buffers_sizes]
-   shl rdx, 2
-   mov r10, rdx
-   add rdx, rdx
-   add rdx, r10
-
-   mov rax, 3 ;read
-   mov rbx, [fd_in]
-   mov rcx, readed_data
-   int 0x80
-
-   mov rdi, GL_ARRAY_BUFFER
-   mov rsi, [buffers_ids+8]
-   call glBindBuffer
-
-   mov rdi, GL_ARRAY_BUFFER
-   mov rsi, r10 ;size in bytes
-   mov rdx, readed_data
-   mov rcx, GL_STATIC_DRAW
-   call glBufferData
-
-   mov rdx, [buffers_sizes]
-   shl rdx, 3
-   mov r10, rdx
-
-   mov rax, 3 ;read
-   mov rbx, [fd_in]
-   mov rcx, readed_data
-   int 0x80
-
-   mov rdi, GL_ARRAY_BUFFER
-   mov rsi, [buffers_ids+16]
-   call glBindBuffer
-
-   mov rdi, GL_ARRAY_BUFFER
-   mov rsi, r10
-   mov rdx, readed_data
-   mov rcx, GL_STATIC_DRAW
-   call glBufferData
-
-   mov rax, 3 ;read
-   mov rbx, [fd_in]
-   mov rcx, readed_data
-   mov rdx, 4
-   int 0x80
-
-   xor rdx, rdx
-   mov edx, [readed_data]
-   mov rdi, rdx
-
-   mov [buffers_sizes+8], rdx
-
-   shl rdx, 2
-
-   mov rax, 3 ;read
-   mov rbx, [fd_in]
-   mov rcx, readed_data
-   int 0x80
-
-   mov rdi, GL_ELEMENT_ARRAY_BUFFER
-   mov rsi, [buffers_ids+24]
-   call glBindBuffer
-
-   mov rdi, GL_ELEMENT_ARRAY_BUFFER
-   mov rsi, [buffers_sizes+8]
-   shl rsi, 2
-   mov rdx, readed_data
-   mov rcx, GL_STATIC_DRAW
-   call glBufferData
-
-   mov eax, 6 ;close
-   mov ebx, [fd_in]
-   int 0x80
+   call _timer_start
+   load_terrain
+   mov rcx, func_name_Load_Terrain
+   call _timer_stop
 
    mov rdi, main_camera
    update_lookat
+
+   setup_graphic_dataptr
 
 
 ;   ____            _             _ _           
@@ -645,6 +980,13 @@ default_sw1:
 
 break_sw1:
 
+;  ____                _           
+; |  _ \ ___ _ __   __| | ___ _ __ 
+; | |_) / _ \ '_ \ / _` |/ _ \ '__|
+; |  _ <  __/ | | | (_| |  __/ |   
+; |_| \_\___|_| |_|\__,_|\___|_|   
+;
+
    mov rdi, 0x4100 ;GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT
    call glClear
 
@@ -671,55 +1013,79 @@ break_sw1:
    call gluLookAt
    add rsp, 8
 
-   mov rdi, GL_VERTEX_ARRAY
-   call glEnableClientState
+   
+   call glPushMatrix
+   movapd xmm0, [main_camera + Camera.eyeX]
+   movapd xmm1, xmm0
+   shufpd xmm1, xmm1, 1
+   movapd xmm2, [main_camera + Camera.eyeZ]
+   call glTranslated
+   bind_texture 0
+   draw_mesh 0
+   call glPopMatrix
 
-   mov rdi, GL_ARRAY_BUFFER
-   mov rsi, [buffers_ids]
-   call glBindBuffer
+   draw_terrain
 
-   mov rdi, 3
-   mov rsi, GL_FLOAT
-   mov rdx, 0
-   mov rcx, 0
-   call glVertexPointer
+   bind_texture 1
+   draw_mesh 1
 
-   mov rdi, GL_TEXTURE_COORD_ARRAY
-   call glEnableClientState
+   ;mov rdi, GL_VERTEX_ARRAY
+   ;call glEnableClientState
 
-   mov rdi, GL_ARRAY_BUFFER
-   mov rsi, [buffers_ids + 16]
-   call glBindBuffer
+   ;mov rdi, GL_NORMAL_ARRAY
+   ;call glEnableClientState
 
-   mov rdi, 2
-   mov rsi, GL_FLOAT
-   mov rdx, 0
-   mov rcx, 0
-   call glTexCoordPointer
+   ;mov rdi, GL_TEXTURE_COORD_ARRAY
+   ;call glEnableClientState
+
+   ;mov rdi, GL_ARRAY_BUFFER
+   ;mov rsi, [buffers_ids]
+   ;call glBindBuffer
+
+   ;mov rdi, GL_ELEMENT_ARRAY_BUFFER
+   ;mov rsi, [buffers_ids+8]  ;buffers_ids
+   ;call glBindBuffer
+
+   ;setup_graphic_dataptr
+
+   ;mov rdi, 3
+   ;mov rsi, GL_FLOAT
+   ;mov rdx, 32
+   ;mov rcx, 0
+   ;call glVertexPointer
+
+   ;mov rdi, GL_FLOAT
+   ;mov rsi, 32
+   ;mov rdx, 12
+   ;call glNormalPointer
+
+   ;mov rdi, 2
+   ;mov rsi, GL_FLOAT
+   ;mov rdx, 32
+   ;mov rcx, 24
+   ;call glTexCoordPointer
 
    ;mov rdi, GL_ARRAY_BUFFER
    ;mov rsi, [buffers_ids+8]
    ;call glBindBuffer
 
-   ;mov rdi, GL_FLOAT
-   ;mov rsi, 0
-   ;mov rdx, 0
-   ;call glNormalPointer
-
-   mov rdi, GL_TEXTURE_2D
-   mov rsi, [textures_ids]
-   call glBindTexture
+   ;mov rdi, GL_TEXTURE_2D
+   ;mov rsi, [textures_ids]
+   ;call glBindTexture
 
 
-   mov rdi, GL_TRIANGLES           ;;WITH VBO NO LONGER NEEDED
-   mov rsi, 0 ;first index
-   mov rdx, [buffers_sizes+8]
-   call glDrawArrays
+   ;mov rdi, GL_TRIANGLES           ;;WITH VBO NO LONGER NEEDED
+   ;mov rsi, 0 ;first index
+   ;mov rdx, [buffers_sizes+16]
+   ;call glDrawArrays
+
+   ;mov rdi, GL_ELEMENT_ARRAY_BUFFER
+   ;mov rsi, [buffers_ids+8]  ;buffers_ids
+   ;call glBindBuffer
 
    ;mov rdi, GL_TRIANGLES
-   ;mov rsi, [buffers_sizes+8]
-   ;shl rsi, 2
-   ;mov rdx, GL_UNSIGNED_SHORT
+   ;mov rsi, [buffers_sizes+24]
+   ;mov rdx, GL_UNSIGNED_INT
    ;mov rcx, 0
    ;call glDrawElements
 
@@ -750,21 +1116,26 @@ end_main_controller:
    call glDeleteBuffers
 
    mov rsp, [stack_start]
-       
-   mov	eax,1             ;system call number (sys_exit)
-   int	0x80              ;call kernel
 
-section	.data
-file_name db 'meshes.ipa', 0
-len_file_name equ $-file_name
+end:
+       
+   mov   eax,1             ;system call number (sys_exit)
+   int   0x80              ;call kernel
+
+section  .data
+mesh_file db 'meshes.ipa', 0
+texture_file db 'textures.ipa', 0
+terrain_file db 'terrain.ipa', 0
+header db 'xkelem01'
 
 window_title db 'IPA project xkelem01', 0
 
-stack_start dq 0
+align 16
+stack_start dq 0  ;init as NULL
 continuer dq 1
 
 align 16
-main_camera dq 2.0, 3.0, 0.0, 0.0, 0.0, 0.0, 0.0, 180.0, 1.0, 0.0
+main_camera dq 2.0, 3.0, 50.0, 0.0, 0.0, 0.0, 0.0, 180.0, 1.0, 0.0
 
 general_switch dd 8, 16, 24, 32, 40, 48, 56, 64
 
@@ -775,13 +1146,24 @@ switch2 dd SDL_SCANCODE_W , SDL_SCANCODE_A , SDL_SCANCODE_S, SDL_SCANCODE_D, SDL
 switch2_labels dq default_sw1, FORWARD_sw2, LEFT_sw1, BACKWARD_sw2, SDL_QUIT_sw1, FORWARD_sw2, ROTATE_LEFT_sw2, BACKWARD_sw2, ROTATE_RIGHT_sw2
 
 verticles dd 1.0, -1.0, 1.0,    -1.0, -1.0, -1.0,    1.0, -1.0, -1.0
-indices dd 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 , 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,33,34,35,36
-test_image dd 1.0, 1.0, 1.0,  0.5,0.5,0.5,  0.0,0.0,0.0,  0.5,0.5,0.5
+indices dd 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 , 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,33,34,35,36
+test_image dd 1.0, 1.0, 1.0,  0.0,0.0,0.0,  0.0,0.0,0.0,  0.0,0.0,0.0
+
+data dd 0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,      1.0,0.0,0.0,0.0,0.0,1.0,1.0,0.0,     0.0,1.0,0.0,0.0,0.0,1.0,0.0,1.0
+;32*3
 
 M_PI_DIV_180 dq 0x3F91D746A2529D39
 
 var0 dq 0x0
-var1 dq 0x3FF0000000000000
+align 16
+var1 dd 0.0, 1.0, 0.0, 0.0
+terrain_size dd 4.0, 0.0, 0.0, 0.0
+mask3 dd -1, 0, -1, -1
+load_terrain_constant100 dd 10.0, 10.0, 10.0, 10.0
+element_terrain_constant1 dd 0, 1, 128, 128
+element_terrain_constant2 dd 129, 1, 1, 2
+element_terrain_constant3 dd 129, 129, 130, 2
+element_terrain_add dd 2, 2, 2, 2
 
 timer_fmt db 'rdsct time difference: %d%d cycles (func %s)', 0xa, 0
 
@@ -792,6 +1174,9 @@ func_name_SDL_CreateWindow db 'SDL_CreateWindow', 0
 func_name_SDL_GL_CreateContext db 'SDL_GL_CreateContext', 0
 func_name_SDL_PollEvent db 'SDL_PollEvent', 0
 func_name_Update_LookAt db 'Update_LookAt', 0
+func_name_Load_Textures db 'Load_Textures', 0
+func_name_Load_Meshes db 'Load_Meshes', 0
+func_name_Load_Terrain db 'Load_Terrain', 0
 
 debug_Uint32_fmt db 'output decimal: %d', 0xa, 0
 debug_double_fmt db 'output double: %f', 0xa, 0
@@ -799,14 +1184,18 @@ debug_double_fmt db 'output double: %f', 0xa, 0
 ;insta_args_gluPerspective dq 
 
 section .bss   ;uninitialised data
-
+align 16
 xmm_something resq 2
 timer_first resq 2 ;first time-stamp for compare (EDX:EAX = 64b)
 SDL_event resb 64 ;event handler
-textures_ids resd 4
-buffers_ids resq 4
-buffers_sizes resq 4
+align 16
+textures_ids resd 16
+buffers_ids resq 16
+buffers_sizes resq 16
+textures_sizes resq 16
 
 fd_out resq 1
 fd_in  resq 1
-readed_data resd 1024
+align 16
+terrain_height resd 16384
+readed_data resd 2097152
